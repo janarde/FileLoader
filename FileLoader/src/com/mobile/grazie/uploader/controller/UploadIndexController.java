0a6cc6065bad15df9d992725fe0e8a6982429109
Jesse_Anarde <Jesse_Anarde@SE-JAnarde-DT.nuance.com>
2012-08-24 15:41:01 -0700

    actually added some views and an index controller

---
+++ FileLoader/src/com/mobile/grazie/uploader/controller/UploadIndexController.java
@@ -0,0 +1,25 @@
+package com.mobile.grazie.uploader.controller;
+
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/uploadfileindex")
+public class UploadIndexController {
+
+	// display on get request
+	@RequestMapping(method = RequestMethod.GET)
+	public String showRegistration(Map<String, String> model) {
+		return "uploadForm";
+	}
+	
+	//process the form
+	@RequestMapping(method = RequestMethod.POST)
+	public String processRegistration(BindingResult result) {
+		return "uploadForm";
+	}
+	
+}
