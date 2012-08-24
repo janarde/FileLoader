48e26185dde7dd45f3dec61f1a8b3d9eb4d7ec27
Jesse_Anarde <jesse.anarde@gmail.com>
2012-08-24 11:01:21 -0700

    created

---
+++ FileLoader/src/com/mobile/grazie/uploader/controller/UploadController.java
@@ -0,0 +1,40 @@
+package com.mobile.grazie.uploader.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+
+import com.mobile.grazie.uploader.model.UploaderModel;
+
+@Controller
+@RequestMapping(value = "/upload")
+public class UploadController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String getUploadForm(Model model) {
+		model.addAttribute(new UploaderModel());
+		return "upload/uploadForm";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String create(UploaderModel model, BindingResult result) {
+		if (result.hasErrors()) {
+			for (ObjectError error : result.getAllErrors()) {
+				System.err.println("Error = " + error.getCode() + " - " + error.getDefaultMessage());
+			}
+			return "upload/uploadForm";
+		}
+		
+	    // Some type of file processing...
+	    System.err.println("-------------------------------------------");
+	    System.err.println("Test upload: " + model.getName());
+	    System.err.println("Test upload: " + model.getFileData().getOriginalFilename());
+	    System.err.println("-------------------------------------------");
+	 
+	    return "redirect:/app/";
+	}
+	
+}
---
+++ FileLoader/src/com/mobile/grazie/uploader/model/UploaderModel.java
@@ -0,0 +1,22 @@
+package com.mobile.grazie.uploader.model;
+
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+public class UploaderModel {
+
+	private String name;
+	private CommonsMultipartFile fileData;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public CommonsMultipartFile getFileData() {
+		return fileData;
+	}
+	public void setFileData(CommonsMultipartFile file) {
+		this.fileData = file;
+	}
+	
+}
